6c711c7d58ca26947c6dd301bebfdea5f04c6698
Vishal Vallapu <108678923+VishalVallapu@users.noreply.github.com>
2024-10-28 12:15:21 -0500

    Update Createlogin.sql

--- Database/Createlogin.sql
+++ Database/Createlogin.sql
@@ -1,1 +1,14 @@
+create login vishal with password = '12345678';
 
+create user vishal for login vishal;
+
+drop login vishal;
+drop user vishal;
+
+select 
+s.name as schemaName,
+t.name as tableName
+from sys.tables t
+inner join sys.schemas s
+on t.schema_id = s.schema_id
+where s.name = 'SalesLT'; 
